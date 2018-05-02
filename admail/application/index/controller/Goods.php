a1109f15d5d9108376e1b57fd9eb906091668722
yang <qyys2007@126.com>
2018-05-03 01:43:58 +0800

    test

--- admail/application/index/controller/Goods.php
+++ admail/application/index/controller/Goods.php
@@ -13,6 +13,7 @@ namespace app\index\controller;
 use app\admin\model\Goods as GoodsModel;
 use app\admin\model\Order as OrderModel;
 use app\admin\model\Comment as CommentModel;
+
 /**
  * 前台首页控制器
  * @package app\index\controller
@@ -22,8 +23,25 @@ class Goods extends Home
     public function index()
     {
 
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> '.config("dolphin.product_name").' '.config("dolphin.product_version").'<br/><span style="font-size:30px">hshshh</span></p></div>';
+        $id = input('id');
+        if(empty($id)){
+            $this->error('id error');
+        }
 
+        $info = GoodsModel::get($id);
+        if(empty($info['status'])){
+            $this->error('商品还没上架 ');
+        }
+        // 默认跳转模块
+        if (config('home_default_module') != 'index') {
+            $this->redirect(config('home_default_module'). '/index/index');
+        }
+        $c_list = CommentModel::getList($id);
+        $this->assign('info',$info);
+        $this->assign('c_list',$c_list);
+        $this->assign('c_count',count($c_list));
+
+        return $this->fetch();
     }
 
     public function save(){
--- admail/application/index/controller/Index.php
+++ admail/application/index/controller/Index.php
@@ -22,23 +22,7 @@ class Index extends Home
 {
     public function index()
     {
-        $id = input('id');
-        if(empty($id)){
-            $this->error('id error');
-        }
 
-        $info = GoodsModel::get($id);
-        if(empty($info['status'])){
-            $this->error('商品还没上架 ');
-        }
-        // 默认跳转模块
-        if (config('home_default_module') != 'index') {
-            $this->redirect(config('home_default_module'). '/index/index');
-        }
-        $c_list = CommentModel::getList($id);
-        $this->assign('info',$info);
-        $this->assign('c_list',$c_list);
-        $this->assign('c_count',count($c_list));
   
         return $this->fetch();
     }
